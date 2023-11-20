ace9112a578747431978e6af9ada8ae7b4ad56b6
jaron-bauers <jaron.bauers@gmail.com>
2023-11-20 14:59:55 -0700

    feat: added level 3

--- Assets/Scripts/MenuNavigator.cs
+++ Assets/Scripts/MenuNavigator.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuNavigator : MonoBehaviour
 {
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Level_Select");
